9440eb9ee02214f95cfa9f9b24d5af6517c1cb10
sanjay-ab <67157359+sanjay-ab@users.noreply.github.com>
2021-03-05 19:23:15 +0000

    Add files via upload

--- PWLibs/Laser_Cooling.py
+++ PWLibs/Laser_Cooling.py
@@ -13,88 +13,126 @@ elif platform == "linux" or "linux2":
 mLi = 6.941*sc.physical_constants["atomic mass constant"][0] #mass of lithium atom in kg
 gamma = 2*np.pi * 5.92e6 #natural linewidth of Lithium (2pi*hz)
 const1 = 4/gamma**2 
-omega_0 = 2.807389399e+15 #frequency of transition excluding hyperfine shifts
-omega = 2.807387462245734e15 #frequency of transtition including the hyperfine shifts at zero field
+omega_0 = 2.807389399e+15 #frequency of 2S1/2 -> 2P3/2 transition excluding hyperfine shifts
+omega = 2.807387462245734e15 #frequency of F=2 -> F'=3 laser transtition including the hyperfine shifts at zero field
+omega_r = 2.80739257e15 #frequency of F=1 -> F'=2 repump transition including the hyperfine shifts at zero field
 laser_origins = [np.array([-1e-2,0,0]),np.array([1e-2,0,0]),np.array([0,-1e-2,0]),np.array([0,1e-2,0]),np.array([0,0,-1e-2]),np.array([0,0,1e-2])] #origin of lasers
 laser_vectors = [np.array([1,0,0]),np.array([-1,0,0]),np.array([0,1,0]),np.array([0,-1,0]),np.array([0,0,1]),np.array([0,0,-1])] #directions of lasers
 Zeeman_Shift = zeeman_shift() #define class used to calculate zeeman shifts for different states.
 
-def calculate_probability(atom, mF, B, dt, omega_L, S_0, W_0, k_mod):
-    probabilities = np.zeros((6,3)) #array of the probabilities of the 3 transitions (delta mF of 1, 0 ,-1) from each of the 6 lasers
+def mF_state(F,mF):
+    """Calculates the mF state the atom falls to depending on the mF value of the upper state it decays from and the F value of the ground state it falls to"""
+    if F==2: #atom return to F=2 state
+        if mF == 3: #atom transitioned to mF=3 state
+            return 2  #atom returns to either mF=+2,-2 state.
+        elif mF == -3:
+            return -2
+        elif mF == 2: #atom transitioned to mF=2 state
+            return  np.random.randint(1,3) #atom returns to either the mF=+-1 state or mF=+-2 state with a 50/50 chance of either
+        elif mF == -2:
+            return - np.random.randint(1,3)
+        else:
+            return mF + np.random.randint(-1,2) #mF can change by any of +1,0,-1
 
-    for i in range(6):
+    else: #atom returns to F=1 state
+        if mF == 2: #atom transitioned to mF=2 state
+            return  1 #atom returns to mF=+1
+        elif mF == -2:
+            return - 1
+        elif mF == 1:
+            return np.random.randint(0,2)
+        elif mF == -1:
+            return - np.random.randint(0,2)
+        else: 
+            return np.random.randint(-1,2) #mF can be any of +1,0,-1
 
-        #calculate W_ij
-        Bmod = np.linalg.norm(B)
-        theta = np.arccos(np.dot(B,laser_vectors[i])/Bmod) #angle in radians between the magnetic field and the laser k vector
-        k = k_mod * laser_vectors[i] #k vector of laser
-        atom[:3] -= laser_origins[i] #shift atom by distance to laser
+def calculate_probability(atom, mF, F, B, dt, omega_L, S_0, W_0, k_mod):
+    probabilities = np.zeros((6,3,3)) #array of the probabilities of the 3 transitions (delta mF of 1, 0 ,-1) from each of the 6 lasers and each F level (F+1, F, F-1)
+    
+    for k in range(3):
+        #loop through each F value 
+        for i in range(6): 
+            #loop through each laser
+            #calculate W_ij
+            Bmod = np.linalg.norm(B)
+            theta = np.arccos(np.dot(B,laser_vectors[i])/Bmod) #angle in radians between the magnetic field and the laser k vector
+            k_L = k_mod * laser_vectors[i] #k vector of laser
+            atom[:3] -= laser_origins[i] #shift atom by distance to laser
 
-        if i in (0,1): #x lasers
-            #x and y direction lasers are LCP wrt propagation direction
-            index1, index2, index3 = 0, 1, 2 #define indicies for calculating waist of laser
-            pol = -1 # define laser as LCP polarized (LCP photon possesses +hbar of momentum so effects the mF+1 transition?)
+            if i in (0,1): #x lasers
+                #x and y direction lasers are LCP wrt propagation direction
+                index1, index2, index3 = 0, 1, 2 #define indicies for calculating waist of laser
+                pol = 1 # define laser as LCP polarized (LCP photon possesses +hbar of momentum so effects the mF+1 transition?)
 
-        elif i in (2,3): #+y laser
-            index1, index2, index3 = 1, 2, 0
-            pol = -1 # define laser as LCP polarized
+            elif i in (2,3): #+y laser
+                index1, index2, index3 = 1, 2, 0
+                pol = 1 # define laser as LCP polarized
 
-        else: #+z laser
-            #z direction lasers are RCP wrt propagation direction
-            index1, index2, index3 = 2, 0, 1
-            pol = 1 # define laser as RCP polarized 
+            else: #+z laser
+                #z direction lasers are RCP wrt propagation direction
+                index1, index2, index3 = 2, 0, 1
+                pol = 1 # define laser as RCP polarized 
 
-        W = W_0 * np.sqrt(1 + ( 2*atom[index1] / (k_mod * W_0**2) )**2 ) #calculate waist. 
-        S = S_0 * (W_0/W)**2 * np.exp( -2 * (atom[index2]**2 + atom[index3]**2 ) / W**2 ) #calculate I/Isat using eqn for gaussian beam intensity
+            W = W_0 * np.sqrt(1 + ( 2*atom[index1] / (k_mod * W_0**2) )**2 ) #calculate waist. 
+            S = S_0 * (W_0/W)**2 * np.exp( -2 * (atom[index2]**2 + atom[index3]**2 ) / W**2 ) #calculate I/Isat using eqn for gaussian beam intensity
 
-        E = 0.5 * np.array([pol - np.cos(theta), -np.sqrt(2) * np.sin(theta), pol + np.cos(theta)]) #rotated electric field vector in spherical basis
+            E = 0.5 * np.array([pol - np.cos(theta), -np.sqrt(2) * np.sin(theta), pol + np.cos(theta)]) #rotated electric field vector in spherical basis
 
-        Wi = np.array([abs(E[0]),abs(E[1]), abs(E[2])])**2 #calculate Wij weights for transitions (mf=+1 , 0 , -1)
+            Wi = np.array([abs(E[0]),abs(E[1]), abs(E[2])])**2 #calculate Wij weights for transitions (mf=+1 , 0 , -1)
 
-        lower_state_shift = Zeeman_Shift.S_energy(int(mF),Bmod) #calculate zeeman shift of the current atom state #L=0 J=1/2 F=2
+            lower_state_shift = Zeeman_Shift.S_energy(int(F),int(mF),Bmod) #calculate zeeman shift of the current atom state #L=0 J=1/2 F=2
 
-        upper_state_shifts = Zeeman_Shift.P_adjacent_energies(int(mF), Bmod) #calculate zeeman shift of possible upper atoms states (deltamF=+1,0,-1)
+            upper_state_shifts = Zeeman_Shift.P_adjacent_energies(int(F-k+1),int(mF), Bmod) #calculate zeeman shift of possible upper atoms states (deltamF=+1,0,-1)
 
-        const2 = (omega_L - omega_0) - np.dot(k,atom[3:]) #Delta - dot(k,v)
+            const2 = (omega_L - omega_0) - np.dot(k_L,atom[3:]) #Delta - dot(k,v)
 
-        probabilities[i] = (gamma*S*dt/2) * np.array(Wi/(1+S*Wi + const1 * (const2 + 2*np.pi*(lower_state_shift - upper_state_shifts))**2)) #calculate probability of transition to each possible upper state.
+            for j in range(3):
+                if isnan(upper_state_shifts[j]):
+                    probabilities[i,j,k] = 0 #if the transition is impossible
+                else:
+                    probabilities[i,j,k] = (gamma*S*dt/2) * np.array(Wi[j]/(1+S*Wi[j] + const1 * (const2 + 2*np.pi*(lower_state_shift - upper_state_shifts[j]))**2)) #calculate probability of transition to each possible upper state.
 
     #below samples the probabilities to find whether a transition occurs
-    
-    prob_1 = probabilities[0,0]
-    prob_2 = 0
     random_number = np.random.rand()
 
+    prob_1 = probabilities[0,0,0]
+
     if 0 < random_number < prob_1: #see if the first possible transition occurs (1st laser with deltamF=+1)
-        return (0,0)
+        return (0,0,0)
 
-    for i in range(6): #loop through each index of laser
-        for j in range(3): #loop through each index of transition
+    for k in range(3): #loop through each index of F transition
+        for i in range(6): #loop through each index of laser
+            for j in range(3): #loop through each index of mF transition
 
-            if j==2: 
-                if i==5:
-                    return(np.nan, np.nan) #no transition occurs so return NaN
+                if j==2: 
+                    if i==5:
+                        if k==2:
+                            return (np.nan, np.nan, np.nan)
+                        prob_2 = prob_1 + probabilities[0 ,0 ,k+1]
+                        x , y , z = 0, 0 , k+1
+                    else:
+                        prob_2 = prob_1 + probabilities[i+1 ,0 ,k]
+                        x , y , z = i + 1, 0 , k  #have to shift indicies since the transition we are examining is on the next line of the array
 
-                prob_2 = prob_1 + probabilities[i+1,0]
-                x , y = i + 1, 0 #have to shift indicies since the transition we are examining is on the next line of the array
-
-            else:
-                prob_2 = prob_1 + probabilities[i, j+1]
-                x , y = i , j + 1 #we examine if the transition described by (i , j + 1) occurs.
-
-
-            if prob_1 <= random_number < prob_2:
-                return x,y #return the transition that occurs x=laser index y=transition index.
-
-            prob_1 = prob_2
+                else:
+                    prob_2 = prob_1 + probabilities[i, j+1, k]
+                    x , y , z= i , j + 1 , k  #we examine if the transition described by (i , j + 1) occurs.
 
 
-def Laser(atom_array, trap,dt, detuning, S_0, W_0):
+                if prob_1 <= random_number < prob_2:
+                    return x,y,z #return the transition that occurs x=laser index y=transition index.
+
+                prob_1 = prob_2
+
+
+def Laser(atom_array, trap, dt, detuning, detuning_r, S_0, W_0):
     indicies = []
     B = trap.rQuery(atom_array) #vector magnetic field at the position of each atom
-    omega_L = omega + detuning
-    k_mod = omega_L/sc.c #modulus of detuned lasers k vectors
-    #W_0 = np.sqrt( (9e-6)/2 + np.sqrt(81e-12 - (16e-4)/(k_mod**2) )/2 ) #assume waist of laser is at centre of trap assume laser is 1cm away from the trap and waist is 3mm there
+    omega_pump = omega + detuning
+    k_L = omega_pump/sc.c #modulus of detuned lasers k vectors
+
+    omega_repump = omega_r + detuning_r
+    k_r = omega_repump/sc.c #modulus of detuned lasers k vectors
 
     for i, atom in enumerate(atom_array):
 
@@ -102,7 +140,14 @@ def Laser(atom_array, trap,dt, detuning, S_0, W_0):
             indicies.append(i) #add index to list to remove row from array at end of loop
             continue #continue to next atom
 
-        laser_index, transition_index = calculate_probability(atom[:6], atom[6], B[i], dt, omega_L, S_0, W_0, k_mod) #check whether a transition occurs for the atom
+        if atom[7] == 2: #if atom in F=2 state
+            omega_L = omega_pump #set frequency to pump laser frequency as atom only interacts with this laser
+            k_mod = k_L
+        else: #atom in F=1 state
+            omega_L = omega_repump #frequency of repump laser as atom only interacts with repump laser
+            k_mod = k_r
+
+        laser_index, transition_index_mF, transition_index_F = calculate_probability(atom[:6], atom[6], atom[7], B[i], dt, omega_L, S_0, W_0, k_mod) #check whether a transition occurs for the atom
 
         if isnan(laser_index):
             continue #if no interaction then skip to next atom
@@ -115,18 +160,21 @@ def Laser(atom_array, trap,dt, detuning, S_0, W_0):
         k_s = np.array([np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)]) #select k vector of photon spontaneously emitted using a random uniform distribution
 
         atom_array[i,3:6] += ((sc.hbar*k_mod)/mLi) * (laser_vectors[laser_index] + k_s) #change velocity of atom due to interaction event
-        upper_state = atom[6] + (1 - transition_index) #mF value of the upper state the atom transitions to
+        upper_state_mF = atom[6] + (1 - transition_index_mF) #mF value of the upper state the atom transitions to
+        upper_state_F = atom[7] + (1- transition_index_F)
 
-        if upper_state == 3: #atom transitioned to mF=3 state
-            atom_array[i,6] = 2  #atom returns to either mF=+2,-2 state.
-        elif upper_state == -3:
-            atom_array[i,6] = -2
-        elif upper_state == 2: #atom transitioned to mF=2 state
-            atom_array[i,6] =  np.random.randint(1,3) #atom returns to either the mF=+-1 state or mF=+-2 state with a 50/50 chance of either
-        elif upper_state == -2:
-            atom_array[i,6] =  - np.random.randint(1,3)
-        else: #atom is in mF=+1,0,-1
-            atom_array[i,6] = upper_state + np.random.randint(-1,2) #mF can change by any of +1,0,-1 
+        if upper_state_F == 3: #if atom transitioned to F=3 state
+            atom_array[i,6] = mF_state(2, upper_state_mF)
+        elif upper_state_F == 0:
+            atom_array[i,6] = mF_state(1, upper_state_mF) #don't have to set F state of atoms if it excites to either F=3 (0) since it must have been in F=2 (1) so it does not change 
+        else: #F=1,2 states
+            if np.random.rand()<(1/3):
+                atom_array[i,7] = 2 #1/3 chance to return to F=2 state
+                atom_array[i,6] = mF_state(2, upper_state_mF)
+            else:
+                atom_array[i,7] = 1 #2/3 chance to return to F=1 state. From branching ratios 
+                atom_array[i,6] = mF_state(1, upper_state_mF)
+
 
     atom_array = np.delete(atom_array,indicies,axis=0) #delete rows of atoms that have left the trap
 
--- PWLibs/Zeeman_Shift.py
+++ PWLibs/Zeeman_Shift.py
@@ -14,7 +14,12 @@ Pdata2=  np.genfromtxt(f'Input{slash}B.txt', delimiter=',')
 Pdata3=  np.genfromtxt(f'Input{slash}C.txt', delimiter=',')  
 Pdata4=  np.genfromtxt(f'Input{slash}D.txt', delimiter=',')  
 Pdata5=  np.genfromtxt(f'Input{slash}E.txt', delimiter=',')  
-Pdata6=  np.genfromtxt(f'Input{slash}F.txt', delimiter=',') 
+Pdata6=  np.genfromtxt(f'Input{slash}F.txt', delimiter=',')
+Pdata7=  np.genfromtxt(f'Input{slash}G.txt', delimiter=',')  
+Pdata8=  np.genfromtxt(f'Input{slash}H.txt', delimiter=',')  
+Pdata9=  np.genfromtxt(f'Input{slash}I.txt', delimiter=',')  
+Pdata10=  np.genfromtxt(f'Input{slash}J.txt', delimiter=',')  
+Pdata11=  np.genfromtxt(f'Input{slash}K.txt', delimiter=',')
 Pdata12=  np.genfromtxt(f'Input{slash}L.txt', delimiter=',')  
 Pdata13=  np.genfromtxt(f'Input{slash}M.txt', delimiter=',')  
 Pdata14=  np.genfromtxt(f'Input{slash}N.txt', delimiter=',')  
@@ -25,13 +30,32 @@ Pdata17=  np.genfromtxt(f'Input{slash}Q.txt', delimiter=',')
 P25data1= np.genfromtxt(f'Input{slash}A25.txt', delimiter=',')  
 P25data2=  np.genfromtxt(f'Input{slash}B25.txt', delimiter=',')  
 P25data3=  np.genfromtxt(f'Input{slash}C25.txt', delimiter=',')  
-P25data4=  np.genfromtxt(f'Input{slash}D25.txt', delimiter=',')  
+P25data4=  np.genfromtxt(f'Input{slash}D25.txt', delimiter=',')
 P25data5=  np.genfromtxt(f'Input{slash}E25.txt', delimiter=',')  
+P25data6=  np.genfromtxt(f'Input{slash}F25.txt', delimiter=',')  
+P25data7=  np.genfromtxt(f'Input{slash}G25.txt', delimiter=',')  
+P25data8=  np.genfromtxt(f'Input{slash}H25.txt', delimiter=',')
 P25data9=  np.genfromtxt(f'Input{slash}I25.txt', delimiter=',')  
-P25data10=  np.genfromtxt(f'Input{slash}J25.txt', delimiter=',')   
-P25data14=  np.genfromtxt(f'Input{slash}N25.txt', delimiter=',')  
+P25data10=  np.genfromtxt(f'Input{slash}J25.txt', delimiter=',')
+P25data11=  np.genfromtxt(f'Input{slash}K25.txt', delimiter=',')  
+P25data12=  np.genfromtxt(f'Input{slash}L25.txt', delimiter=',')  
+P25data13=  np.genfromtxt(f'Input{slash}M25.txt', delimiter=',')
+P25data14=  np.genfromtxt(f'Input{slash}N25.txt', delimiter=',')
+P25data15=  np.genfromtxt(f'Input{slash}O25.txt', delimiter=',')  
+P25data16=  np.genfromtxt(f'Input{slash}P25.txt', delimiter=',')  
+P25data17=  np.genfromtxt(f'Input{slash}Q25.txt', delimiter=',') 
+
 
 New_Pdata1 = np.concatenate((Pdata1,P25data1))
+New_Pdata2= np.concatenate((Pdata7,P25data17))
+New_Pdata3 = np.concatenate((Pdata11,P25data13))
+New_Pdata4 = np.concatenate((Pdata10,P25data12))
+New_Pdata5 = np.concatenate((Pdata6,P25data16))
+New_Pdata6 = np.concatenate((Pdata15,P25data6))
+New_Pdata7 = np.concatenate((Pdata16,P25data7))
+New_Pdata8 = np.concatenate((Pdata17,P25data8))
+New_Pdata9 = np.concatenate((Pdata9,P25data11))
+New_Pdata10 = np.concatenate((Pdata8,P25data15))
 New_Pdata11 = np.concatenate((Pdata2,P25data2))
 New_Pdata12 = np.concatenate((Pdata3,P25data3))
 New_Pdata13 = np.concatenate((Pdata4,P25data4))
@@ -39,6 +63,20 @@ New_Pdata14 = np.concatenate((Pdata5,P25data5))
 New_Pdata15 = np.concatenate((Pdata14,P25data9))
 New_Pdata16 = np.concatenate((Pdata13,P25data10))
 New_Pdata17 = np.concatenate((Pdata12,P25data14))
+
+"""Modelling the P state"""
+
+Pmodel00 = interp1d(New_Pdata1, New_Pdata2, kind='cubic',bounds_error=False) #F=0 mF=0
+
+Pmodel1_1 = interp1d(New_Pdata1, New_Pdata3, kind='cubic',bounds_error=False) #F=1 mF=-1
+Pmodel10 = interp1d(New_Pdata1, New_Pdata4, kind='cubic',bounds_error=False) #F=1 mF=0
+Pmodel11 = interp1d(New_Pdata1, New_Pdata5, kind='cubic',bounds_error=False) #F=1 mF=1
+
+Pmodel2_2 = interp1d(New_Pdata1, New_Pdata6, kind='cubic',bounds_error=False) #F=2 mF=-2
+Pmodel2_1 = interp1d(New_Pdata1, New_Pdata7, kind='cubic',bounds_error=False) #F=2 mF=-1
+Pmodel20 = interp1d(New_Pdata1, New_Pdata8, kind='cubic',bounds_error=False) #F=2 mF=0
+Pmodel21 = interp1d(New_Pdata1, New_Pdata9, kind='cubic',bounds_error=False) #F=2 mF=1
+Pmodel22 = interp1d(New_Pdata1, New_Pdata10, kind='cubic',bounds_error=False) #F=2 mF=2
 
 Pmodel3_3 = interp1d(New_Pdata1, New_Pdata11, kind='cubic',bounds_error=False) #F=3 mF=-3
 Pmodel3_2 = interp1d(New_Pdata1, New_Pdata12, kind='cubic',bounds_error=False) #F=3 mF=-2
@@ -51,9 +89,10 @@ Pmodel33 = interp1d(New_Pdata1, New_Pdata17, kind='cubic',bounds_error=False) #F
 """Modeling the S-state"""
 
 BreitRabiE = np.genfromtxt(f'Input{slash}2S_BreitRabi.csv', delimiter=',') #get breit rabi data for 2S1/2 state
-#Smodel11 = interp1d(BreitRabiE[:,0], BreitRabiE[:,1], kind='cubic',bounds_error=False) #F=1 mF=1
-#Smodel10 = interp1d(BreitRabiE[:,0], BreitRabiE[:,2], kind='cubic',bounds_error=False) #F=1 mF=0
-#Smodel1_1 = interp1d(BreitRabiE[:,0], BreitRabiE[:,3], kind='cubic',bounds_error=False) #F=1 mF=-1
+Smodel11 = interp1d(BreitRabiE[:,0], BreitRabiE[:,1], kind='cubic',bounds_error=False) #F=1 mF=1
+Smodel10 = interp1d(BreitRabiE[:,0], BreitRabiE[:,2], kind='cubic',bounds_error=False) #F=1 mF=0
+Smodel1_1 = interp1d(BreitRabiE[:,0], BreitRabiE[:,3], kind='cubic',bounds_error=False) #F=1 mF=-1
+
 Smodel2_2 = interp1d(BreitRabiE[:,0], BreitRabiE[:,4], kind='cubic',bounds_error=False) #F=2 mF=-2
 Smodel2_1 = interp1d(BreitRabiE[:,0], BreitRabiE[:,5], kind='cubic',bounds_error=False) #F=2 mF=-1
 Smodel20 = interp1d(BreitRabiE[:,0], BreitRabiE[:,6], kind='cubic',bounds_error=False) #F=2 mF=0
@@ -65,14 +104,51 @@ class zeeman_shift:
     
     def __init__(self):
         
-        self._S = [Smodel22, Smodel21, Smodel20, Smodel2_1, Smodel2_2] #array for models for S levels
+        self._S2 = [Smodel22, Smodel21, Smodel20, Smodel2_1, Smodel2_2] #array for models for S levels F=2
+        self._S1 = [Smodel11, Smodel10, Smodel1_1] #array for models for S levels F=1
 
-        self._P = [Pmodel33, Pmodel32, Pmodel31, Pmodel30, Pmodel3_1, Pmodel3_2, Pmodel3_3] #holds models for P levels
+        self._P3 = [Pmodel33, Pmodel32, Pmodel31, Pmodel30, Pmodel3_1, Pmodel3_2, Pmodel3_3] #holds models for P levels F=3
+        self._P2 = [Pmodel22, Pmodel21, Pmodel20, Pmodel2_1, Pmodel2_2] #holds models for P levels F=2
+        self._P1 = [Pmodel11, Pmodel10, Pmodel1_1] #holds models for P levels F=1
+        self._P0 = [Pmodel00] #holds models for P levels F=0
 
-    def S_energy(self, mF, Bfield):
+    def S_energy(self, F, mF, Bfield):
+        if F==2:
+            return self._S2[2-mF](Bfield) #return the zeeman shift (2pi*Hz) for 2S1/2 state for the given mF value
+        elif F==1:
+            return self._S1[1-mF](Bfield)
 
-        return self._S[2-mF](Bfield) #return the zeeman shift (2pi*Hz) for 2S1/2 state for the given mF value
-
-    def P_adjacent_energies(self, mF, Bfield):
+    def P_adjacent_energies(self, F, mF, Bfield):
         #doesn't work for mF=+-3 but that should never be an input, max input should be mF=+-2
-        return (self._P[-1 + 3-mF](Bfield), self._P[3-mF](Bfield), self._P[1 + 3-mF](Bfield)) #return the zeeman shifts (2pi*Hz) for the 2P3/2 state for adjacent mF levels as to the one specified (Deltamf=+1,0,-1)
+        if F==3:
+            return (self._P3[-1 + 3-mF](Bfield), self._P3[3-mF](Bfield), self._P3[1 + 3-mF](Bfield)) #return the zeeman shifts (2pi*Hz) for the 2P3/2 state for adjacent mF levels as to the one specified (Deltamf=+1,0,-1)
+
+        elif F==2:
+            if mF==2:
+                return (np.nan, self._P2[2-mF](Bfield), self._P2[1 + 2-mF](Bfield)) #+1 transitions is impossible
+            elif mF==-2:
+                return (self._P2[-1 + 2-mF](Bfield), self._P2[2-mF](Bfield), np.nan) #-1 transition is impossible
+            else:
+                return (self._P2[-1 + 2-mF](Bfield), self._P2[2-mF](Bfield), self._P2[1 + 2-mF](Bfield))
+
+        elif F==1:
+            if mF==2:
+                return (np.nan, np.nan, self._P1[1 + 1-mF](Bfield))
+            elif mF==-2:
+                return (self._P1[-1 + 1-mF](Bfield), np.nan, np.nan)
+            elif mF==1:
+                return (np.nan, self._P1[1-mF](Bfield), self._P1[1 + 1-mF](Bfield))
+            elif mF==-1:
+                return (self._P1[-1 + 1-mF](Bfield), self._P1[1-mF](Bfield), np.nan)
+            else:
+                return (self._P1[-1 + 1-mF](Bfield), self._P1[1-mF](Bfield), self._P1[1 + 1-mF](Bfield))
+
+        elif F==0:
+            #should only have to consider max mF=+-1 since this state is only accessed from the F=1 state
+
+            if mF==1:
+                return (np.nan, np.nan, self._P0[1 + -mF](Bfield))
+            elif mF==-1:
+                return (self._P0[-1 + -mF](Bfield), np.nan, np.nan)
+            else:
+                return (np.nan, self._P0[-mF](Bfield), np.nan)
